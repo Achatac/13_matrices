3b76ee586284a5aebbf05dd220880507966d160d
Achatac <achatac@unjbg.edu.pe>
2024-07-15 18:42:06 -0500

    implementamos la funcion suma y solicitamos al usuario el ingreso del tamano de la matriz

---
+++ matrices2.cpp
@@ -0,0 +1,21 @@
+//Suma de elementos de la diagonal secundaria de una matriz cuadrada
+#include <iostream>
+using namespace std;
+int main (){
+
+    int suma (int n, int matriz[n][100]);
+
+    int suma (int n, int matriz[n][100]){
+        
+    }
+
+
+    int n;
+    cout << "Ingrese el tamanio de la matriz cuadrada: "; cin >> n;
+    int matriz[n][100];
+
+
+
+
+    return 0;
+}
