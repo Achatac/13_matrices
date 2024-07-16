db1bed65419248ff88cf0fa398224d584c642542
Achatac <achatac@unjbg.edu.pe>
2024-07-15 19:23:32 -0500

    declaramos la funcion generaMatriz y funcion principal

---
+++ matrices3.cpp
@@ -0,0 +1,19 @@
+//matriz de dimension n x 3n
+#include <iostream>
+using namespace std;
+
+void generaMatriz (int n);
+
+void generaMatriz (int n){
+
+}
+
+int main (){
+
+    int n;
+    cout << "Ingrese el valor de 'n' para generar una matriz de orden (n x 3n): "; cin >> n;
+
+    
+    return 0;
+}
+
