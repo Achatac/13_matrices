5f6aa2d38d7dbd1d36fb0df5d739f9f03afa32e1
Achatac <achatac@unjbg.edu.pe>
2024-07-15 19:05:21 -0500

    implementamos mas las estructuras que nos permiten solicitar cada valor de la matriza eimprimirla por la pantalla

--- matrices2.cpp
+++ matrices2.cpp
@@ -18,7 +18,21 @@ int main (){
     cout << "Ingrese el tamanio de la matriz cuadrada: "; cin >> n;
     int matriz[n][100];
 
+    cout << "Ingrese los elementos de la matriz:" << endl;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << "[" << i + 1 << "] [" << j + 1 << "] "<< " : ";  cin >> matriz[i][j];
+        }
+    }
 
+    cout << "La matriz es:" << endl;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << matriz[i][j] << " ";
+        }
+        cout << endl;
+    }
+    
 
 
     return 0;
